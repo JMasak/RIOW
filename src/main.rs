f9715a09bb18227bda2ac71bec6e78cee4e788ae
Jan Masak <jmasak@me.com>
2023-11-05 19:06:22 +0100

    6.1 Surface normals

--- src/main.rs
+++ src/main.rs
@@ -19,21 +19,30 @@ const VIEWPORT_HEIGHT: f32 = 2.0;
 const VIEWPORT_WIDTH: f32 = VIEWPORT_HEIGHT * (IMAGE_WIDTH as f32 / IMAGE_HEIGHT as f32);
 const FOCAL_LENGTH: f32 = 1.0;
 
-fn hit_sphere(center: &Vec3, radius: f32, r: &Ray) -> bool
+fn hit_sphere(center: &Vec3, radius: f32, r: &Ray) -> f32
 {
     let oc = r.origin() - center;
     let a = Vec3::dot(&r.direction(), &r.direction());
     let b = 2.0 * Vec3::dot(&oc, &r.direction());
     let c = Vec3::dot(&oc, &oc) - radius * radius;
     let discriminant = b * b - 4.0*a*c;
-    discriminant >= 0.0
+    if discriminant < 0.0
+    {
+        return -1.0
+    }
+    else 
+    {
+        return (-b - discriminant.sqrt() ) / (2.0*a);
+    }
 }
 
 fn ray_color(r: &Ray) -> Vec3
 {
-    if hit_sphere(&Vec3::with_position(0.0, 0.0, -1.0), 0.5, &r)
+    let t = hit_sphere(&Vec3::with_position(0.0, 0.0, -1.0), 0.5, &r);
+    if t > 0.0 
     {
-        return Vec3::with_color(1.0, 0.0, 0.0);
+        let N = Vec3::unit_vector(&(r.at(t) - Vec3::with_position(0.0,0.0,-1.0)));
+        return 0.5*Vec3::with_color(N.x()+1.0, N.y()+1.0, N.z()+1.0);
     }
     let unit_direction = Vec3::unit_vector(&r.direction());
     let a = 0.5*(unit_direction.y() + 1.0);
