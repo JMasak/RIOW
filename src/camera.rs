296a7fa38606ee4533ac047280659a7b22188762
Jan Masak <jmasak@me.com>
2023-11-12 22:02:13 +0100

    7 Camera

---
+++ src/camera.rs
@@ -0,0 +1,87 @@
+use std::{time::Instant, fs::File, io::Write};
+use crate::{hitable::{HitableList}, ray::Ray, vec3::Vec3, interval::Interval};
+
+
+pub struct Camera {
+    pub aspect_ratio: f32,      // Ratio of image width over height
+    pub image_width: usize,     // Rendered image width in pixel count
+    image_height: usize,        // Rendered image height
+    center: Vec3,               // Camera center
+    pixel00_loc: Vec3,          // Location of pixel 0, 0
+    pixel_delta_u: Vec3,        // Offset to pixel to the right
+    pixel_delta_v: Vec3         // Offset to pixel below
+}
+
+impl Camera {
+    pub fn new() -> Self {
+        Camera { 
+            aspect_ratio: 1.0, 
+            image_width: 100, 
+            image_height: 100, 
+            center: Vec3::with_position(0.0, 0.0, 0.0), 
+            pixel00_loc: Vec3::with_position(-0.09, 0.09, 1.0), 
+            pixel_delta_u: Vec3::with_position(0.02, 0.0, 0.0) ,
+            pixel_delta_v: Vec3::with_position(0.0, -0.02, 0.0)
+        }
+    }
+    pub fn render(&mut self, world: &HitableList) {
+        self.initialize();
+        let mut f = File::create("test.ppm").unwrap();
+        write!(f, "P3\n{} {}\n255\n", self.image_width, self.image_height).unwrap();
+        let start_time = Instant::now();
+
+        for j in 0..self.image_height
+        {
+            print!("\rScanlines remaining: {}     ", (self.image_height - j));
+            for i in 0..self.image_width
+            {
+                let pixel_center = &self.pixel00_loc + (i as f32 * &self.pixel_delta_u) + (j as f32 * &self.pixel_delta_v);
+                let ray_direction = pixel_center - &self.center;
+                let r = Ray::new(&self.center, &ray_direction);
+
+                let c = self.ray_color(&r, &world);
+                write!(f, "{}\n", c).unwrap();
+            }
+        }
+        let elapsed_time = start_time.elapsed();
+        print!("\rDone! Raytracing took: {:?}\n", elapsed_time);
+    }
+
+    fn initialize(&mut self) {
+        self.center = Vec3::with_position(0.0, 0.0, 0.0);
+        self.image_height = match (self.image_width as f32 / self.aspect_ratio) as usize {
+            0 => 1,
+            x => x
+        };
+
+        // Determine viewport dimensions.
+        let focal_length = 1.0;
+        let viewport_height = 2.0;
+        let viewport_width = viewport_height * self.aspect_ratio;
+
+        // Calculate the vectors across the horizontal and down the vertical viewport edges.
+        let viewport_u = Vec3::with_position(viewport_width, 0.0, 0.0);
+        let viewport_v = Vec3::with_position(0.0, -viewport_height, 0.0);
+
+        // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+        self.pixel_delta_u = &viewport_u / self.image_width as f32;
+        self.pixel_delta_v = &viewport_v / self.image_height as f32;
+
+        // Calculate the location of the upper left pixel.
+        let viewport_upper_left = &self.center - Vec3::with_position(0.0, 0.0, focal_length) - &viewport_u/2.0 - &viewport_v/2.0;
+        self.pixel00_loc = &viewport_upper_left + &(&self.pixel_delta_u + &self.pixel_delta_v) * 0.5;
+    }
+
+    fn ray_color(&self, r: &Ray, world: &HitableList) -> Vec3 {
+        match world.hit(r, Interval::with_min_max(0.0, std::f32::INFINITY)) {
+            Some(rec)=> {
+                0.5 * (rec.normal + Vec3::with_color(1.0, 1.0, 1.0))
+            },
+            None => {
+                let unit_direction = Vec3::unit_vector(&r.direction());
+                let a = 0.5*(unit_direction.y() + 1.0);
+                Vec3::with_color(1.0, 1.0, 1.0) * (1.0 - a) + a * Vec3::with_color(0.5, 0.7, 1.0)
+            }
+        } 
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,15 @@ mod ray;
 mod hitable;
 mod sphere;
 mod interval;
+mod camera;
 
-use std::{fs::File, io::Write};
-use std::time::Instant;
-
-use interval::Interval;
+use camera::Camera;
 
 use crate::sphere::Sphere;
 use crate::vec3::Vec3;
-use crate::ray::Ray;
 use crate::hitable::HitableList;
 
 // constants
-const INFINITY: f32 = f32::INFINITY;
 const PI: f32 = std::f32::consts::PI;
 
 //utility functions
@@ -23,71 +19,15 @@ pub fn degrees_to_radians(degrees: f32) -> f32 {
     degrees * PI / 180.0
 }
 
-//define Picture
-const ASPECT_RATION: f32 = 16.0 / 9.0;
-const IMAGE_WIDTH: usize = 800;
-const IMAGE_HEIGHT: usize = match (IMAGE_WIDTH as f32 / ASPECT_RATION) as usize {
-    0 => 1,
-    x => x
-};
-
-//define Camera
-const VIEWPORT_HEIGHT: f32 = 2.0;
-const VIEWPORT_WIDTH: f32 = VIEWPORT_HEIGHT * (IMAGE_WIDTH as f32 / IMAGE_HEIGHT as f32);
-const FOCAL_LENGTH: f32 = 1.0;
-
-fn ray_color(r: &Ray, world: &HitableList) -> Vec3
-{
-    match world.hit(r, Interval::with_min_max(0.0, INFINITY)) {
-        Some(rec)=> {
-            0.5 * (rec.normal + Vec3::with_color(1.0, 1.0, 1.0))
-        },
-        None => {
-            let unit_direction = Vec3::unit_vector(&r.direction());
-            let a = 0.5*(unit_direction.y() + 1.0);
-            Vec3::with_color(1.0, 1.0, 1.0) * (1.0 - a) + a * Vec3::with_color(0.5, 0.7, 1.0)
-        }
-    } 
-}
-
 fn raytrace() {
-    let camera_center = Vec3::with_position(0.0, 0.0, 0.0);
-
     // world
     let mut world = HitableList::new();
     world.add(Box::new(Sphere::new(Vec3::with_position(0.0, -100.5, -1.0), 100.0)));
     world.add(Box::new(Sphere::new(Vec3::with_position(0.0, 0.0, -1.0), 0.5)));
-
-    // Calculate the vectors across the horizontal and down the vertical viewport edges.
-    let viewport_u = Vec3::with_position(VIEWPORT_WIDTH, 0.0, 0.0);
-    let viewport_v = Vec3::with_position(0.0, -VIEWPORT_HEIGHT, 0.0);
-
-    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
-    let pixel_delta_u = &viewport_u / IMAGE_WIDTH as f32;
-    let pixel_delta_v = &viewport_v / IMAGE_HEIGHT as f32;
-
-    // Calculate the location of the upper left pixel.
-    let viewport_upper_left = &camera_center - Vec3::with_position(0.0, 0.0, FOCAL_LENGTH) - &viewport_u/2.0 - &viewport_v/2.0;
-    let pixel00_loc = &viewport_upper_left + &(&pixel_delta_u + &pixel_delta_v) * 0.5;
-    let mut f = File::create("test.ppm").unwrap();
-    write!(f, "P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT).unwrap();
-    let start_time = Instant::now();
-
-    for j in 0..IMAGE_HEIGHT
-    {
-        print!("\rScanlines remaining: {}     ", (IMAGE_HEIGHT - j));
-        for i in 0..IMAGE_WIDTH
-        {
-            let pixel_center = &pixel00_loc + (i as f32 * &pixel_delta_u) + (j as f32 * &pixel_delta_v);
-            let ray_direction = pixel_center - &camera_center;
-            let r = Ray::new(&camera_center, &ray_direction);
-
-            let c = ray_color(&r, &world);
-            write!(f, "{}\n", c).unwrap();
-        }
-    }
-    let elapsed_time = start_time.elapsed();
-    print!("\rDone! Raytracing took: {:?}\n", elapsed_time);
+    let mut cam = Camera::new();
+    cam.aspect_ratio = 16.0/9.0;
+    cam.image_width = 720;
+    cam.render(&world);
 }
 
 fn main() {
